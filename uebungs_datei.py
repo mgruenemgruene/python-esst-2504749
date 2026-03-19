6beba8357eb9e6ded6224eefb15baf3681f2568f
mgruenemgruene <markus.gruene@outlook.de>
2026-03-19 18:55:54 +0000

    fertig

--- uebungs_datei.py
+++ uebungs_datei.py
@@ -15,25 +15,21 @@ else:
   computer_zug = 'Papier'
 print('Ich habe zufällig', computer_zug, 'gewählt.')
 
-# Gewinnermittlung Stein
-if auswahl == 'Stein' and computer_zug == 'Stein':
-  print('Unentschieden!') # Stein gg. Stein
+# Gewinnermittlung unentschieden
+if auswahl == computer_zug:
+  print("Unentschieden!")
+
+# Gewinnermittlung
 if auswahl == 'Stein' and computer_zug == 'Schere':
   print('Du hast gewonnen!') # Stein gg Schere
 if auswahl == 'Stein' and computer_zug == 'Papier':
   print('Ich habe gewonnen!') # Stein gg Papier
 
-# Gewinnermittlung Schere
-if auswahl == 'Schere' and computer_zug == 'Schere':
-  print('Unentschieden!') # SSchere gg. Schere
 if auswahl == 'Schere' and computer_zug == 'Stein':
   print('Ich habe gewonnen!') # Schere gg Stein
 if auswahl == 'Schere' and computer_zug == 'Papier':
   print('Du hast gewonnen!') # Schere gg Papier
 
-# Gewinnermittlung Papier
-if auswahl == 'Papier' and computer_zug == 'Papier':
-  print('Unentschieden!') # Papier gg. Papier
 if auswahl == 'Papier' and computer_zug == 'Stein':
   print('Du hast gewonnen!') # Papier gg Stein
 if auswahl == 'Papier' and computer_zug == 'Schere':
