2dbb34b40eed2a557cab8ab8304e4eb04ed738b9
mgruenemgruene <markus.gruene@outlook.de>
2026-03-19 18:36:25 +0000

    update

--- uebungs_datei.py
+++ uebungs_datei.py
@@ -5,18 +5,16 @@ print('Hallo Welt!')
 import random
 
 auswahl = input('Wähle Stein, Schere oder Papier: ')
-print('Du hast', auswahl, 'gewählt!')
+print('Du hast', auswahl, 'gewählt.')
 
 # 0=Stein / 1=Schere / 2=Papier
 zufallszahl = random.randint(0, 2)
 if (zufallszahl == 0):
-  print('Ich habe zufällig Stein gewählt!')
+  computer_zug = 'Stein'
 elif (zufallszahl == 1):
-  print('Ich habe zufällig Schere gewählt!')
+  computer_zug = 'Schere'
 else:
-  print('Ich habe zufällig Papier gewählt!')
+  computer_zug = 'Papier'
+print('Ich habe zufällig', computer_zug, 'gewählt.')
 
 # Gewinnermittlung
-if (auswahl == Stein) and (zufallszahl == 0):
-  print('Unentschieden!') # Stein gegen Stein
-  
