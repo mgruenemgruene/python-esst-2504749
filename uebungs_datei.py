544dc5b78b223347a65a94d5a9a5084bd1165569
mgruenemgruene <markus.gruene@outlook.de>
2026-03-19 17:24:01 +0000

    uebung

--- uebungs_datei.py
+++ uebungs_datei.py
@@ -1,3 +1,11 @@
 # Das ist ein Kommentar in Ihrer Uebungsdatei
 
 print('Hallo Welt!')
+
+import random
+
+auswahl = input('Wähle Stein, Schere oder Papier: ')
+print('Du hast', auswahl, 'gewählt!')
+
+zufallszahl = random.randint(1, 3)
+print('Ich habe zufällig', zufallszahl, 'gewählt!')
